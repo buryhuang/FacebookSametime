c389786bf7ef856121bc2ff3cf201ae6e2f55964
Lion User <User@Lions-Mac.local>
2012-12-03 20:24:58 -0600

    Initial Commit

---
+++ FacebookSametime/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookSametime
+//
+//  Created by Lion User on 03/12/2012.
+//  Copyright (c) 2012 B&W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
