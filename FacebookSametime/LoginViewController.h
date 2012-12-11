a1fd1d4f2658ac2b0bbc69d306ab6aac082bf3d6
Lion User <User@Lions-Mac.local>
2012-12-11 01:11:46 -0600

    Added Auto-Login

---
+++ FacebookSametime/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  FacebookSametime
+//
+//  Created by Lion User on 11/12/2012.
+//  Copyright (c) 2012 B&W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface LoginViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+
+@end
---
+++ FacebookSametime/LoginViewController.m
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.m
+//  FacebookSametime
+//
+//  Created by Lion User on 11/12/2012.
+//  Copyright (c) 2012 B&W. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    if(!FBSession.activeSession.isOpen) {
+        [self performSegueWithIdentifier:@"LoginIntoFacebook" sender:self];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
